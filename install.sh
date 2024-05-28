5c58f6044bf3bd59d8d559b2f9ff7f3ef84e00cb
Will Ginsberg <will.j.ginsberg@gmail.com>
2024-05-28 11:11:17 -0500

    fix docker setup for local development

--- install.sh
+++ install.sh
@@ -10,10 +10,8 @@ yum -y update
 
 yum -y install docker git
 
-# curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/bin/docker-compose
 sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 
-# chmod +x /usr/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 rm -r -f ~/ai-starter
