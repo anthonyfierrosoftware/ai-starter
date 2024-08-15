89afb0931ea0cc173fcc3b0d90dca59c9d7761e9
jordan_meyler <jordan.meyler@gmail.com>
2024-08-15 15:29:05 -0400

    updating new hf model configs

--- backend/aiModule/models/llmConfig.py
+++ backend/aiModule/models/llmConfig.py
@@ -25,14 +25,16 @@ MODELS = {
     
     "mistral-large-latest": "mistral-large-latest",
     "open-mistral-nemo":"open-mistral-nemo",
+    
     "meta-llama/Llama-2-70b-chat-hf": "meta-llama/Llama-2-70b-chat-hf",
+    "meta-llama/Llama-2-7b-chat-hf":"meta-llama/Llama-2-7b-chat-hf"
 }
 
 LLM_MODEL = {
     "OPEN_AI": ["gpt-3.5-turbo", "gpt-4-turbo","gpt-4o","gpt-4o-mini"],
     "CLAUDE": ["claude-3-opus-20240229","claude-3-5-sonnet-20240620", "claude-3-sonnet-20240229","claude-3-haiku-20240307"],
     "MISTRAL": ["mistral-large-latest","open-mistral-nemo"],
-    "HUGGINGFACE": ["meta-llama/Llama-2-70b-chat-hf"],
+    "HUGGINGFACE": ["meta-llama/Llama-2-70b-chat-hf","meta-llama/Llama-2-7b-chat-hf"],
 }
 
 
